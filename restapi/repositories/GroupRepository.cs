9ba5025a2acf669947b63d9a64e5a68c4ea5aaa8
Miguel Angel Balderas Balderas <mbalderas08@alumnos.uaq.mx>
2024-11-05 17:08:34 -0600

    feat: delete and create group

--- restapi/repositories/GroupRepository.cs
+++ restapi/repositories/GroupRepository.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using RestApi.Infrastructure.Mongo;
@@ -31,6 +32,20 @@ public class GroupRepository : IGroupRepository
         }
     }
 
+    public async Task<GroupModel> GetByNameAsync(string name, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var filter = Builders<GroupEntity>.Filter.Regex(group => group.Name, new BsonRegularExpression(name, "i"));
+            var group = await _groups.Find(filter).FirstOrDefaultAsync(cancellationToken);
+            return group.ToModel();
+        }
+        catch (FormatException)
+        {
+            return null;
+        }
+    }
+
     public async Task<IList<GroupModel>> GetAllByNameAsync(string name, int pageNumber, int pageSize, string orderBy, CancellationToken cancellationToken)
     {
         var filter = Builders<GroupEntity>.Filter.Regex(group => group.Name, new BsonRegularExpression(name, "i"));
@@ -41,5 +56,26 @@ public class GroupRepository : IGroupRepository
         }
         var groups = await _groups.Find(filter).Skip(pageSize * (pageNumber - 1)).Limit(pageSize).Sort(sort).ToListAsync(cancellationToken);
         return groups.Select(group => group.ToModel()).ToList();
+    }
+
+    public async Task DeleteByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        var filter = Builders<GroupEntity>.Filter.Eq(group => group.Id, id);
+
+        await _groups.DeleteOneAsync(filter, cancellationToken);
+    }
+
+    public async Task<GroupModel> CreateAsync(string name, Guid[] users, CancellationToken cancellationToken)
+    {
+        var group = new GroupEntity
+        {
+            Name = name,
+            Users = users,
+            CreatedAt = DateTime.UtcNow,
+            Id = ObjectId.GenerateNewId().ToString()
+        };
+
+        await _groups.InsertOneAsync(group, new InsertOneOptions(), cancellationToken);
+        return group.ToModel();
     }
 }
